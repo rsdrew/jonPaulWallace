b9313e13e5cf7c4614ea84c2e98d7edb6f737802
rsdrew <rsdrew@umich.edu>
2024-04-01 23:20:58 -0400

    Added contact section

---
+++ run_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set environment variables
+export JEKYLL_ENV=development
+
+# Run Jekyll
+bundle exec jekyll serve --watch
---
+++ run_prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set environment variables
+export JEKYLL_ENV=production
+
+# Run Jekyll
+bundle exec jekyll serve --watch 
