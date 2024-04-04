a04825808053f81ef06049296163a46120f62f8e
rsdrew <rsdrew@umich.edu>
2024-04-03 22:55:41 -0400

    fix css again

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,5 @@ module.exports = {
   content: ["./_site/index.html"],
   css: ["./_site/assets/css/styles.css", "./_site/assets/css/main.css"],
   output: "./_site/assets/css",
-  safelist: "slide-in--activate"
+  safelist: ["slide-in--activate"]
 };
