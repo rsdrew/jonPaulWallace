f7166f6a1c336ff3206dddbe249afac743955e10
rsdrew <rsdrew@umich.edu>
2024-04-03 21:46:32 -0400

    Updated output path again

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   content: ["./_site/index.html"],
   css: ["./_site/assets/css/styles.css", "./_site/assets/css/main.css"],
-  output: "./_site/assets/css/",
+  output: "./_site/assets/css",
 };
