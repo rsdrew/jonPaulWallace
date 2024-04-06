c3de26a6008a996f28d72366c1980079316b8766
rsdrew <rsdrew@umich.edu>
2024-04-06 15:46:55 -0400

    wip Added Tina CMS

--- tina/config.ts
+++ tina/config.ts
@@ -17,36 +17,150 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: "_src",
+    publicFolder: "./",
   },
   media: {
     tina: {
       mediaRoot: "",
-      publicFolder: "_src",
+      publicFolder: "./assets",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: "about",
+        label: "About Section",
+        path: "_data/about",
+        format: "yaml",
         fields: [
           {
-            type: "string",
             name: "title",
             label: "Title",
-            isTitle: true,
-            required: true,
+            type: "string",
           },
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
+            name: "aboutIntro",
+            label: "Intro Text",
+            type: "string"
           },
-        ],
+          {
+            name: "aboutPhoto",
+            label: "Photo",
+            type: "image"
+          },
+          {
+            name: "aboutShowMoreButtonText",
+            label: "Show More Button Text",
+            type: "string"
+          },
+          {
+            name: "aboutHideInfoButtonText",
+            label: "Hide Info Button Text",
+            type: "string"
+          },
+          {
+            name: "aboutBlurb",
+            label: "About Blurbs",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                name: "highlightedBlurb1",
+                label: "Highlighted Text Blurb 1",
+                type: "string"
+              },
+              {
+                name: "blurb1",
+                label: "Blurb 1",
+                type: "string"
+              },
+              {
+                name: "highlightedBlurb2",
+                label: "Highlighted Text Blurb 2",
+                type: "string"
+              },
+              {
+                name: "blurb2",
+                label: "Blurb 2",
+                type: "string"
+              },
+              {
+                name: "blurbImage",
+                label: "Blurb Image",
+                type: "image"
+              }
+            ]
+          },
+          {
+            name: "aboutCTAText",
+            label: "Call to Action Button Text",
+            type: "string",
+          },
+          {
+            name: "aboutCTALink",
+            label: "Call to Action Button Link",
+            type: "string"
+          }
+        ]
+      },
+      {
+        name: "photos",
+        label: "Photos Section",
+        path: "_data/photos",
+        format: "yaml",
+        fields: [
+          {
+            name: "photosSection",
+            label: "Event",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                name: "name",
+                label: "Event Name",
+                type: "string"
+              },
+              {
+                name: "images",
+                label: "Images",
+                type: "image",
+                list: true
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "links",
+        label: "Social Media and Streaming Platforms",
+        path: "_data/links",
+        format: "yaml",
+        fields: [
+          {
+            name: "app",
+            label: "App",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                name: "name",
+                label: "Name",
+                type: "string"
+              },
+              {
+                name: "link",
+                label: "link",
+                type: "string"
+              },
+              {
+                name: "icon",
+                label: "Image",
+                type: "image"
+              }
+            ]
+          }
+        ]
       },
     ],
   },
