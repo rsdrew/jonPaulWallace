36fe858d9285a2762a566a79069fe37014aede09
rsdrew <rsdrew@umich.edu>
2024-07-06 16:33:01 -0400

    Added netlify function to sign up for email list

---
+++ assets/js/emailing-list-form.js
@@ -0,0 +1,52 @@
+const successMessage = document.getElementById("emailing-list-success-message");
+const errorMessage = document.getElementById("emailing-list-error-message");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("emailing-list-form");
+
+  form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+  
+    const email = document.getElementById("emailing-list-email");
+    const data = { email: email };
+
+    try {
+      debugger;
+      const response = await fetch("https://main--transcendent-marzipan-2b3200.netlify.app/.netlify/functions/subscribe", {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const result = await response.json();
+
+      resetMessages();
+
+      if (response.ok) {
+        displaySuccessMessage();
+      }
+      else {
+        displayErrorMessage();
+      }
+    }
+    catch {
+      displayErrorMessage();
+    }
+  });
+
+});
+
+function displaySuccessMessage() {
+  successMessage.classList.remove("hidden");
+}
+
+function displayErrorMessage() {
+  errorMessage.classList.remove("hidden");
+}
+
+function resetMessages() {
+  successMessage.classList.add("hidden");
+  errorMessage.classList.add("hidden");
+}
