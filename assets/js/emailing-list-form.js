23924cbe0166597a7405097b0960d23c2cb73133
rsdrew <rsdrew@umich.edu>
2024-07-07 01:53:58 -0400

    Fix for adding users to emailing list

--- assets/js/emailing-list-form.js
+++ assets/js/emailing-list-form.js
@@ -1,51 +1,123 @@
-const successMessage = document.getElementById("emailing-list-success-message");
-const errorMessage = document.getElementById("emailing-list-error-message");
+const formClassName = "emailing-list-form";
+const emailInputClassName = "emailing-list-email";
+const successMessageClassName = "emailing-list-success-message";
+const errorMessageClassName = "emailing-list-error-message";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("emailing-list-form");
+const forms = document.querySelectorAll(`.${formClassName}`);
 
-  form.addEventListener("submit", async function(event) {
-    event.preventDefault();
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  resetMessagesForForm(form);
   
-    const email = document.getElementById("emailing-list-email").value;
-    const data = { email: email };
+  const email = form.querySelector("input[name='email']").value;
+  const data = { email: email };
 
-    try {
-      const response = await fetch("https://transcendent-marzipan-2b3200.netlify.app/.netlify/functions/subscribe", {
-        method: "POST",
-        headers: {
-          "Content-Type": 'application/json',
-        },
-        body: JSON.stringify(data)
-      });
+  try {
 
-      const result = await response.json();
+    setButtonAsSubmittingForForm(form);
 
-      resetMessages();
+    //const debugUrl = "http://localhost:8888/.netlify/functions/subscribe";
+    const prodUrl = "https://main--transcendent-marzipan-2b3200.netlify.app/.netlify/functions/subscribe";
+    const response = await fetch(prodUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": 'application/json',
+        "credentials": "include"
+      },
+      body: JSON.stringify(data)
+    });
 
-      if (response.ok) {
-        displaySuccessMessage();
-      }
-      else {
-        displayErrorMessage();
-      }
+    const result = await response.json();
+
+    enableButtonForForm(form);
+
+    if (response.ok) {
+      displaySuccessMessageForForm(form, result.message);
     }
-    catch {
-      displayErrorMessage();
+    else {
+      displayErrorMessageForForm(form, result.message);
     }
-  });
+  }
+  catch {
+    displayErrorMessageForForm(form, result.message);
+  }
+}
 
-});
+forms.forEach((form) => {
+  form.addEventListener('submit', handleFormSubmit);
+})
 
-function displaySuccessMessage() {
+function displaySuccessMessageForForm(form, message) {
+  const successMessage = getSuccessMessageForForm(form);
   successMessage.classList.remove("hidden");
+  setTimeout(() => {
+    successMessage.classList.add("slide-in--activate");
+  }, 1);
+  successMessage.innerHTML = message;
 }
 
-function displayErrorMessage() {
+function displayErrorMessageForForm(form, message) {
+  const errorMessage = getErrorMessageForForm(form);
   errorMessage.classList.remove("hidden");
+  setTimeout(() => {
+    errorMessage.classList.add("slide-in--activate");
+  }, 1);
+  errorMessage.innerHTML = message;
 }
 
-function resetMessages() {
+function resetMessagesForForm(form) {
+  const successMessage = getSuccessMessageForForm(form);
+  const errorMessage = getErrorMessageForForm(form);
   successMessage.classList.add("hidden");
   errorMessage.classList.add("hidden");
+  successMessage.classList.remove("slide-in--activate");
+  errorMessage.classList.remove("slide-in--activate");
+}
+
+function getSuccessMessageForForm(form) {
+  return form.querySelector(`.${successMessageClassName}`);
+}
+
+function getErrorMessageForForm(form) {
+  return form.querySelector(`.${errorMessageClassName}`);
+}
+
+function getButtonForForm(form) {
+  return form.querySelector("button[type='submit']");
+}
+
+function setButtonAsSubmittingForForm(form) {
+  const button = getButtonForForm(form);
+  button.disabled = true;
+  button.innerHTML = "Signing up...";
+}
+
+function enableButtonForForm(form) {
+  const button = getButtonForForm(form);
+  button.disabled = false;
+  button.innerHTML = "Sign up";
+}
+
+function handleEmailInputOnChange(event) {
+  const emailInput = event.target;
+
+  const form = getFormFromDescendant(emailInput);
+
+  resetMessagesForForm(form);
+}
+
+const emailInputs = document.querySelectorAll(`.${emailInputClassName}`);
+
+emailInputs.forEach((input) => {
+  input.addEventListener('input', handleEmailInputOnChange)
+});
+
+function getFormFromDescendant(element) {
+  while (!element.classList.contains(`${formClassName}`)) {
+    element = element.parentElement;
+  }
+  return element;
 }
--- netlify/functions/subscribe/subscribe.js
+++ netlify/functions/subscribe/subscribe.js
@@ -1,5 +1,19 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+
+  debugger;
+
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      }
+    }
+  }
+
   const email = JSON.parse(event.body).email;
   const mailchimpApiKey = process.env.MAILCHIMP_API_KEY;
   const listId = process.env.MAILCHIMP_LIST_ID;
@@ -8,36 +22,44 @@ const handler = async (event) => {
   const url = `https://${dc}.api.mailchimp.com/3.0/lists/${listId}/members/`;
 
   const subscriberData = {
-    "email_address": email,
-    "status": 'subscribed'
+    email_address: email,
+    status: 'subscribed'
   };
 
   try {
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
         'Authorization': `Bearer ${mailchimpApiKey}`,
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(subscriberData)
     });
 
+    const responseData = await response.json();
+
+    // Member already added
+    if (response.status === 400 && responseData.title === "Member Exists") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "You're already signed up!" })
+      }
+    }
+
     if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
+      throw new Error(`Error: ${responseData}`);
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Subscription successful!' })
+      body: JSON.stringify({ message: "Thanks for signing up! See you at the next show!" })
     };
 
   } catch (error) {
     return { 
       statusCode: 500,
       body: JSON.stringify({
-        error: error.toString(),
-        event: event,
-        body: JSON.parse(event.body).email,
+        message: "Something went wrong. Please try again."
       })
     }
   }
